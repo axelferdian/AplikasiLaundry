3e02892fc46ee7e9e8fdf860f7117a5417d934cb
axelferdian <axelferdian99@gmail.com>
2016-10-15 21:23:23 +0700

    radiobutton

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl5007/aplikasilaundry/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl5007/aplikasilaundry/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     EditText etNama, etAlamat, etNomor;
     TextView tvHasil;
     Button btOk;
+    RadioButton rbCB, rbCK, rbCS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
         etNomor = (EditText) findViewById(R.id.editTextNomor);
         tvHasil = (TextView) findViewById(R.id.textView);
         btOk = (Button) findViewById(R.id.buttonOK);
+        rbCB = (RadioButton) findViewById(R.id.radioButtonCB);
+        rbCK = (RadioButton) findViewById(R.id.radioButtonCK);
+        rbCS = (RadioButton) findViewById(R.id.radioButtonCS);
 
         findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,10 +42,30 @@ public class MainActivity extends AppCompatActivity {
         String alamat = etAlamat.getText().toString();
         String nomor = etNomor.getText().toString();
 
+        String hasil = null;
+        int harga = 0;
+        if (rbCB.isChecked()) {
+            hasil = rbCB.getText().toString();
+            harga = 5000;
+        } else if (rbCK.isChecked()) {
+            hasil = rbCK.getText().toString();
+            harga = 10000;
+        } else if (rbCS.isChecked()) {
+            hasil = rbCS.getText().toString();
+            harga = 15000;
+        } else {
+            hasil = "Anda belum memilih jenis laundry";
+            harga = 0;
+        }
+
+        int total = harga;
+
         if (isValid()) {
             tvHasil.setText("Nama Anda : " + nama +
                     "\nAlamat : " + alamat +
-                    "\nNomor Telepon : " + nomor);
+                    "\nNomor Telepon : " + nomor +
+                    "\nJenis Laundry : " + hasil +
+                    "\nTotal Bayar : " + String.valueOf(total));
         }
     }
 
